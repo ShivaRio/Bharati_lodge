f86a27f31d166bd25ba949f587df38e40f4b53e2
ShivaRio <sivabalaji2002@gmail.com>
2024-05-10 16:41:21 +0530

    updated Files

--- Frontend/Home.js
+++ Frontend/Home.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState, useRef } from 'react';
 import { View, Animated, DrawerLayoutAndroid, Image, Text, StyleSheet, Button, TextInput, ActivityIndicator , TouchableHighlight, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 import { DataTable, Modal, Portal, FlatList, Provider } from 'react-native-paper';
 import axios from "axios";
+import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 
   const Home = ({navigation}) => {
     const[data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(false);
+    const tableHead = ['Head', 'Head2', 'Head3', 'Head4'];
 
     const navigateToMember = () => {
       navigation.navigate('Member');
@@ -40,19 +43,46 @@ finally{
 
 fetchSubscriptionData ();
 
-
     },[]);
         
   return (
     
        <View style={styles.container}>
-      <Text style={styles.centeredTitle}>BHARTI LODGE</Text>
+
+<View style={styles.header}>
+            <Icon name="chevron-left" size={25} style={{ color: '#ffffff', marginTop:10, }} onPress={navigation.goBack} />
+            <Text style={styles.headerText}>SUBSCRIPTION DETAILS</Text>
+            <Icon name="sort-variant" size={28} style={{ color: '#ffffff', marginTop:-25, marginLeft:320 }} />
+            </View>
+
+
+
+<Table borderStyle={{ borderColor: 'transparent' }}>
+<Row data={tableHead} style={styles.head} textStyle={styles.text} />
+
+
+
+
+</Table>
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <Text style={styles.centeredTitle}>BHARTI LODGE</Text>
       <View>
     <Image source={require('./img/img3.jpg')} resizeMode="contain" style={styles.logo} />
-</View>
+</View> */}
 
 
-<ScrollView horizontal showsVerticalScrollIndicator>
+{/* <ScrollView horizontal showsVerticalScrollIndicator>
         <DataTable style={styles.dataTable}>
          <DataTable.Header style={{backgroundColor:'#660066'}}>
         <DataTable.Title textStyle={{fontSize:15, color:'#ffffff'}} >Degree</DataTable.Title>
@@ -78,7 +108,7 @@ fetchSubscriptionData ();
 ))}
 </ScrollView>
         </DataTable>
-</ScrollView>
+</ScrollView> */}
 
 {isLoading && (
         <View style={styles.loadingContainer}>
@@ -103,60 +133,83 @@ const styles = StyleSheet.create({
     alignItems: 'center',
          
    },
+   header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    backgroundColor: '#660066',
+    
+},
+headerText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#ffffff',
+    marginLeft:80,
+    
+},
+//    imageContainer: {
+//     width: '100%', // Adjust this according to your layout
+//     alignItems: 'center',
+// },
+// logo: {
+//     width: 380, // Adjust width and height as needed
+//     height: 300,
+// },
 
-   imageContainer: {
-    width: '100%', // Adjust this according to your layout
-    alignItems: 'center',
-},
-logo: {
-    width: 380, // Adjust width and height as needed
-    height: 300,
-},
-
-dataTable: {
-  backgroundColor: '#ffeb99',
-  borderWidth: 3,
-  borderLeftWidth: 3,
-  borderRightWidth: 3,
-  marginLeft: 10,
-  marginRight: 20,
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-},
-  scroll:{
-    marginHorizontal: 6,
+// dataTable: {
+//   backgroundColor: '#ffeb99',
+//   borderWidth: 3,
+//   borderLeftWidth: 3,
+//   borderRightWidth: 3,
+//   marginLeft: 10,
+//   marginRight: 20,
+//   flex: 1,
+//   justifyContent: 'center',
+//   alignItems: 'center',
+// },
+//   scroll:{
+//     marginHorizontal: 6,
  
 
-  },
-  centeredTitle: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 28,
-    color: '#660066',
-    marginTop:20,
-  },
-  cell: {
-    flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 21,
-    borderColor: 'black',
-    fontSize: 10,
-    color: '#000000',
-    textAlign: 'center',
-  },
-  loadingText: {
-    marginTop: 10,
-    fontSize: 16,
+//   },
+//   centeredTitle: {
+//     textAlign: 'center',
+//     fontWeight: 'bold',
+//     fontSize: 28,
+//     color: '#660066',
+//     marginTop:20,
+//   },
+//   cell: {
+//     flex: 0.5,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     padding: 21,
+//     borderColor: 'black',
+//     fontSize: 10,
+//     color: '#000000',
+//     textAlign: 'center',
+//   },
+//   loadingText: {
+//     marginTop: 10,
+//     fontSize: 16,
     
-  },
- loadingContainer: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
+//   },
+//  loadingContainer: {
+//     ...StyleSheet.absoluteFillObject,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+//   },
+
+
+container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#808B97' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+  btn: { width: 58, height: 18, backgroundColor: '#78B7BB', borderRadius: 2 },
+  btnText: { textAlign: 'center', color: '#fff' }
+
 });
 
 export default Home;
